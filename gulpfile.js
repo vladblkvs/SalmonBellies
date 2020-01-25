2c3614e4a101d1c45db33077f667fd86910a8601
vladblkvs <gremlinux@gmail.com>
2020-01-25 16:50:45 +0300

    Edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
-var less = require("gulp-less");
+var scss = require("gulp-sass");
 var sourcemap = require("gulp-sourcemaps");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -17,10 +17,10 @@ var del = require("del");
 var server = require("browser-sync").create();
 
 gulp.task("css", function() {
-  return gulp.src("source/less/style.less")
+  return gulp.src("source/less/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(less())
+    .pipe(scss())
     .pipe(postcss([
       autoprefixer()
     ]))
